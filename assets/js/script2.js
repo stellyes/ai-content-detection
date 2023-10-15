07cd5f923f8bf0667255383d58ef182a99fdda1f
Ryan England <me@rengland.org>
2023-10-14 19:39:39 -0700

    Final styling added to search page

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,49 +1,48 @@
 const textAPIkey = "da6d75ca4emsh6141adc1de07170p15d12ajsn08a3f957a221";
-const newsAPIkey = "pub_310941d0b0fa18d49abbe048e6b4f4d748fbe"
-const historyContainer = document.getElementById('history-list');
-const searchBox = document.getElementById('search-bar');
+const newsAPIkey = "pub_310941d0b0fa18d49abbe048e6b4f4d748fbe";
+const historyContainer = document.getElementById("history-list");
+const searchBox = document.getElementById("search-bar");
 
 var searchHistoryList = [];
 var searchHistoryAi = [];
 
-
 function getNews(query) {
-         apiCall = `https://newsdata.io/api/1/news?apikey=pub_310941d0b0fa18d49abbe048e6b4f4d748fbe&q=${query}&size=2`;
-    fetch(apiCall)
-      .then(function (response) {
-        console.log(response);
-        return response.json();
-      }) 
-      .then(function (data) {
+  apiCall = `https://newsdata.io/api/1/news?apikey=pub_310941d0b0fa18d49abbe048e6b4f4d748fbe&q=${query}&size=2`;
+  fetch(apiCall)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.status == "success") {
         console.log(data);
-        if (data.status == "success") {
-            console.log(data)
         //   Get array of articles from data
-          let articles = data.results;
-          // Iterate over articles and append to webpage
-          for (let i = 0; i < articles.length; i++) {
-            fillSearchResults(articles[i]);
-          }
-        } else {
-          console.log(">> API Call to newsapi.org failed");
+        let articles = data.results;
+        // Iterate over articles and append to webpage
+        for (let i = 0; i < articles.length; i++) {
+          fillSearchResults(articles[i]);
         }
-      })
-      .catch(function(error) {
-        console.error('Error fetching data:', error);
-      });
-  }
-  
-  // Generates content for webpage
-  function fillSearchResults(result) {
-    let searchResultsElement = document.getElementById('search-results');
-    let listItem = document.createElement('li')
-    let link = document.createElement('a');
-        link.href = result.link;
-        link.target = '_blank';
-        link.textContent = result.title;
-        listItem.appendChild(link);
-        searchResultsElement.appendChild(listItem); 
-        }
+      }
+    })
+    .catch(function (error) {
+      console.error("Error fetching data:", error);
+    });
+}
+
+// Generates content for webpage
+function fillSearchResults(result) {
+  let searchResultsElement = document.getElementById("search-results-view");
+  let listItem = document.createElement("li");
+  let link = document.createElement("a");
+  link.href = result.link;
+  link.target = "_blank";
+  link.textContent = result.title;
+  listItem.appendChild(link);
+  listItem.setAttribute("id", "search-result");
+  listItem.setAttribute("class", "w3-margin-left w3-round-xlarge");
+  searchResultsElement.appendChild(listItem);
+}
 
 function handleSearch() {
   if (searchBox.value) {
@@ -60,7 +59,7 @@ function addHistory(historyArray, storage) {
   //If search entry has been searched, removes the old entry from history
   if (historyArray.includes(searchedContent)) {
     historyArray.splice(historyArray.indexOf(searchedContent), 1);
-  } 
+  }
   //If history has more than 10 entries, removes the oldest entry
   else if (historyArray.length >= 10) {
     historyArray.splice(historyArray.length - 1, 1);
@@ -74,7 +73,7 @@ function addHistory(historyArray, storage) {
 function updateHistory(historyArray, storage) {
   var storageItem;
   for (var i = 0; i < historyArray.length; i++) {
-    storageItem = storage + ' ' + i;
+    storageItem = storage + " " + i;
     if (historyArray[i] != null) {
       localStorage.setItem(storageItem, historyArray[i]);
     }
@@ -85,7 +84,7 @@ function updateHistory(historyArray, storage) {
 function recallHistory(historyArray, storage) {
   var storageItem;
   for (var i = 0; i < 10; i++) {
-    storageItem = storage + ' ' + i;
+    storageItem = storage + " " + i;
     historyArray[i] = localStorage.getItem(storageItem);
   }
 }
@@ -103,14 +102,14 @@ function renderHistory(historyArray) {
       break;
     }
     historyContainer.lastElementChild.remove();
-    console.log('removed el');
+    console.log("removed el");
   }
   //Initial page rendering
   for (var i = 0; i < historyArray.length; i++) {
     if (historyArray[i] == null) {
       break;
     }
-    historyEntry  = document.createElement("li");
+    historyEntry = document.createElement("li");
     historyEntry.innerHTML = historyArray[i].toString();
     historyContainer.appendChild(historyEntry);
   }
